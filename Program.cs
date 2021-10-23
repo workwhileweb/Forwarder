8e549878d2ceed14f0fa4a7603e964ed0ba1ecc2
workwhileweb <workwhileweb@gmail.com>
2021-10-23 10:40:02 +0700

    2310

--- Program.cs
+++ Program.cs
@@ -1,15 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using Public;
 
 namespace Forwarder
 {
-    class Program
+    public class Config
     {
-        static void Main(string[] args)
+        public string Exe { get; set; }
+        public bool Wait { get; set; } = false;
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
         {
+            Log.Init();
+            var config = Public.Config.LoadConfig<Config>()??new Config();
+
+            //if (!File.Exists(config.Exe)) 
+            //    throw new FileNotFoundException(config.Exe);
+
+            //Environment.CommandLine : exe sẽ có bọc quote
+            //Constant.ExeFile : không bọc quote
+            //Environment.CommandLine.StartsWith(Constant.ExeFile) : remove quote thì ok
+
+            var startInfo = new ProcessStartInfo();
+            // all environment variables of the created process
+            // are inherited from the current process
+            startInfo.EnvironmentVariables["CALLER"] = Constant.ExeFile;
+            // required for EnvironmentVariables to be set
+            startInfo.UseShellExecute = false;
+            startInfo.FileName = config.Exe;
+            startInfo.Arguments = string.Join(" ", args);
+            var proc= Process.Start(startInfo);
+
+            if (proc == null) throw new Exception(nameof(Process));
+
+            if (config.Wait) proc.WaitForExit();
         }
     }
 }
